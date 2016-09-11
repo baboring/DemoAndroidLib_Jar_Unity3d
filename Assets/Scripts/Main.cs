1b193f3b1f8542a7aadfb9711e20f80f572da45e
Benjamin <baboring@gmail.com>
2016-09-11 17:50:20 -0400

    Signed-off-by: Benjamin <baboring@gmail.com> initial-commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Androidtion is to be called");
+
+
+        var ajc = new AndroidJavaClass("com.narith.androidlib.Helper"); //(1)
+        ajc.CallStatic("DoSthInAndroid");                                       //(2)
+        Debug.Log("Androidtion is called");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
